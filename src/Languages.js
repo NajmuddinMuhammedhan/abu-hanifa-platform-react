f6d0fef156c17bbe3fe8fbb9eeb31b952d1a72ad
Najmuddin Muhammedhan <najmuddin.muhammedhan@gmail.com>
2020-12-02 21:50:30 +0300

    Changes

---
+++ src/Languages.js
@@ -0,0 +1,13 @@
+const Languages = {
+	ar: {
+		name: 'عربى',
+	},
+	tr: {
+		name: 'Türkçe',
+	},
+	uz: {
+		name: 'O\'zbekcha',
+	},
+}
+
+export default Languages
---
+++ src/Localization.js
@@ -0,0 +1,94 @@
+const Localization = {
+	ar: {
+		login: {
+			email: 'البريد الإلكتروني',
+			password: 'كلمه السر',
+			enter: 'تسجيل الدخول',
+		},
+		common: {
+			needHelp: 'تحتاج مساعدة؟',
+			forgotPassword: 'هل نسيت كلمة المرور؟',
+			name: 'أكاديمية الإمام أبو حنيفة الإسلامية العالمية',
+			courseName: 'دورة كاملة',
+			courseDesc: 'ندرس ديننا معا. في نهاية الدورة سوف تحصل على شهادة.',
+			read: 'اقرأ',
+		},
+		nav: {
+			profiles: 'إدارة الملفات الشخصية',
+			messages: 'الرسائل',
+			school: 'مدرسة',
+			lessons: 'الدروس',
+			account: 'الحساب',
+			settings: 'إعدادات',
+			logout: 'تسجيل خروج',
+		},
+		school: {
+			stage1: 'المرحلة 1',
+			stage2: 'المرحلة 2',
+			stage3: 'المرحلة 3',
+			stage4: 'المرحلة 4',
+		},
+	},
+	tr: {
+		login: {
+			email: 'E-posta',
+			password: 'Parola',
+			enter: 'Oturum aç',
+		},
+		common: {
+			needHelp: 'Yardıma mı ihtiyacınız var?',
+			forgotPassword: 'Parolanızı mı unuttunuz?',
+			name: 'İmam Ebu Hanife Uluslararası İslam Akademisi',
+			courseName: 'Tam kurs',
+			courseDesc: 'Dinimizi birlikte inceliyoruz. Kursun sonunda bir sertifika alacaksınız.',
+			read: 'Okuyun',
+		},
+		nav: {
+			profiles: 'Profilleri yönetin',
+			messages: 'Mesajlar',
+			school: 'Okul',
+			lessons: 'Dersler',
+			account: 'Hesap',
+			settings: 'Ayarlar',
+			logout: 'Çıkış Yap',
+		},
+		school: {
+			stage1: 'Faz 1',
+			stage2: 'Faz 2',
+			stage3: 'Faz 3',
+			stage4: 'Faz 4',
+		},
+	},
+	uz: {
+		login: {
+			email: 'Email',
+			password: 'Parol',
+			enter: 'Kirish',
+		},
+		common: {
+			needHelp: 'Yordam kerakmi?',
+			forgotPassword: 'Parolni unutdingizmi?',
+			name: 'Imom Abu Hanifa Xalqaro Islom Akademiyasi',
+			courseName: 'To\'liq kurs',
+			courseDesc: 'Dinimizni birgalikda o\'rganamiz. Kurs yakunida sertifikat olasiz.',
+			read: 'O\'qing',
+		},
+		nav: {
+			profiles: 'Profillar boshqaruvi',
+			messages: 'Habarnomalar',
+			school: 'Madrasa',
+			lessons: 'Darslar',
+			account: 'Hisob',
+			settings: 'Sozlamlar',
+			logout: 'Chiqish',
+		},
+		school: {
+			stage1: '1 Bosqich',
+			stage2: '2 Bosqich',
+			stage3: '3 Bosqich',
+			stage4: '4 Bosqich',
+		},
+	},
+}
+
+export default Localization
